92fa6fb48d1df070638af0376667bb58eca3350d
mh <bluesky@example.com>
2015-10-24 00:47:04 +0900

    Save data to hbase using spark-api.

---
+++ hbase/src/main/java/com/blueskyarea/SaveData.java
@@ -0,0 +1,69 @@
+package com.blueskyarea;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Row;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
+import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.PairFunction;
+
+import scala.Tuple2;
+
+public class SaveData {
+	public static void main(String[] args) {
+		// configure HBase
+		Configuration conf = HBaseConfiguration.create();
+		conf.set(TableInputFormat.INPUT_TABLE, "ns:test");
+
+		// Initializing the spark context
+		SparkConf sparkConf = new SparkConf().setAppName("SaveData").setMaster("local");
+		JavaSparkContext jsc = new JavaSparkContext(sparkConf);
+		
+		// Define input data		
+		List<String> data = new ArrayList();
+		data.add("saveData1");
+		data.add("saveData2");
+		JavaRDD<String> dataRdd = jsc.parallelize(data);
+		
+		// new Hadoop API configuration
+		try {			
+			Job newAPIJobConfiguration1 = Job.getInstance(conf);
+			newAPIJobConfiguration1.getConfiguration().set(TableOutputFormat.OUTPUT_TABLE, "ns:test");
+			newAPIJobConfiguration1.setOutputFormatClass(org.apache.hadoop.hbase.mapreduce.TableOutputFormat.class);
+			saveToHBase(dataRdd, newAPIJobConfiguration1.getConfiguration());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void saveToHBase(JavaRDD<String> dataRdd, Configuration conf2) {
+		// create Key, Value pair to store in HBase
+		JavaPairRDD<ImmutableBytesWritable, Put> hbasePuts = dataRdd.mapToPair(new PairFunction<String, ImmutableBytesWritable, Put>() {
+					public Tuple2<ImmutableBytesWritable, Put> call(String row) throws Exception {
+						String rowKey = "saveKey" + row;
+						Put put = new Put(Bytes.toBytes(rowKey));
+						put.add(Bytes.toBytes("data"), Bytes.toBytes("q1"),
+								Bytes.toBytes(row));
+
+						return new Tuple2<ImmutableBytesWritable, Put>(
+								new ImmutableBytesWritable(), put);
+					}
+				});
+		
+		hbasePuts.saveAsNewAPIHadoopDataset(conf2);
+	}
+
+}
